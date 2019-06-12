c43ec0ff1c92ea368393b3b176df175128c03610
wade <1272253421@qq.com>
2019-06-12 17:33:19 +0800

    基础学习

---
+++ python从入门到放弃/变量命令规范.py
@@ -0,0 +1,10 @@
+#字母，数字，下划线
+#不能以数字开头
+#不能与关键字冲突
+import keyword
+print(keyword.kwlist)
+"""['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 
+'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 
+'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda',
+ 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with',
+  'yield']"""
