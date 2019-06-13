1d9058a89331f2fba9cd5bc7a31a5409579d90c7
wade <1272253421@qq.com>
2019-06-13 22:17:42 +0800

    大佬教程

---
+++ python入门大佬教程/生成器、闭包、装饰器/闭包(重要).py
@@ -0,0 +1,18 @@
+def test():
+    print("--1--")
+
+print(test)   #<function test at 0x00000292C227C1E0>
+b=test
+print(b)   #<function test at 0x00000209D0BFC1E0>
+
+test()   #--1--
+b()      #--1--
+
+
+def test1(a,b):
+    def test2(x):
+        return a*x+b
+    return test2      #返回了function类型
+
+a=test1(2,3)   #a指向了test2
+print(a(4))
