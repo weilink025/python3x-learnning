f13ab81e825fb1ded38ae845e917c5f3a264c76d
zhangwei <1272253421@qq.com>
2019-06-17 10:37:10 +0800

    大佬教程

--- python入门大佬教程/生成器、闭包、装饰器/装饰器/对带有返回值的函数进行装饰.py
+++ python入门大佬教程/生成器、闭包、装饰器/装饰器/对带有返回值的函数进行装饰.py
@@ -1,14 +1,17 @@
-def func(fun):
-    print("已经装饰好了")
-    def funcin():
-        fun()
-        print("---funcin----")
-
+#使用装饰器对有返回值的函数进行装饰
+def func(funname):
+    def funcin(*args,**kwargs):
+        print("11111")
+        test1=funname(*args,**kwargs)        #用一个变量接收函数返回值
+        return test1
     return funcin
 
+
+
 @func
-def test():
-    print("---test---")
+def fun1(a,b):
+    print("--我是主函数--")
+    return a+b
 
 
-test()
+print(fun1(11,33))
