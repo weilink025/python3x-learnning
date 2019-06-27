7c113c1afc154d28f2bf9bdbd492091637a9c43e
zhangwei <1272253421@qq.com>
2019-06-27 16:26:26 +0800

    并发

---
+++ python大佬炮制/网络编程/socket-编程/并发服务器/11-协程gevent.py
@@ -0,0 +1,14 @@
+import gevent
+
+def f(n):
+    for i in range(n):
+        print(str(i),gevent.getcurrent())
+        gevent.sleep(1)  #一个协程遇到耗时操作，会先让另一个协程执行
+
+g1=gevent.spawn(f,5)
+g2=gevent.spawn(f,5)
+g3=gevent.spawn(f,5)
+
+g1.join()
+g2.join()
+g3.join()
