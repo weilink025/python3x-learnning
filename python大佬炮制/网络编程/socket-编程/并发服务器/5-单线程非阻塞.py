f166a1e387d5a9d62b5d3f026f874fd653914025
zhangwei <1272253421@qq.com>
2019-06-27 11:48:07 +0800

    并发

---
+++ python大佬炮制/网络编程/socket-编程/并发服务器/5-单线程非阻塞.py
@@ -0,0 +1,48 @@
+from builtins import print
+from socket import *
+from socket import socket
+
+
+def main():
+    tcpsocket = socket(AF_INET,SOCK_STREAM)
+
+    tcpsocket.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
+
+    tcpsocket.setblocking(False)
+
+    tcpsocket.bind(('',8812))
+
+    tcpsocket.listen(10)
+
+    socketList = []
+    while True:
+
+        try:
+
+
+            newSocket,destAddr = tcpsocket.accept()
+
+        except:
+            pass
+        else:
+            print("新客户到来")
+
+            newSocket.setblocking(False)
+            socketList.append(newSocket)
+
+        for newSocket in socketList:
+            try:
+                revcData = newSocket.recv(1024)
+                if revcData:
+                    print(revcData)
+                else:
+                    newSocket.close()
+                    socketList.remove(newSocket)
+
+
+
+            except:
+                pass
+
+if __name__ == "__main__":
+    main()
