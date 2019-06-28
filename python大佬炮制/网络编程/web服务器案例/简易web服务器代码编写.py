5045f793798f5a8cd930e5d2c5dbb9347761318b
wade <1272253421@qq.com>
2019-06-28 22:10:15 +0800

    web服务器

--- python大佬炮制/网络编程/web服务器案例/简易web服务器代码编写.py
+++ python大佬炮制/网络编程/web服务器案例/简易web服务器代码编写.py
@@ -6,29 +6,42 @@ from multiprocessing import Process
 #接收数据，并给客户端发送数据
 def revcfun(newSocket,destAddr):
 
-    while True:
+    recvData = newSocket.recv(1024)
+    if recvData:
 
+        recvGetLocal = recvData.decode().split("\r\n")
+        pathLocal = recvGetLocal[0].split(" ")
+        getfile = pathLocal[1]
+        htmlRootDir = 'html'
 
-        recvData = newSocket.recv(1024)
-        if recvData:
+        try:
+            sendfile = open(htmlRootDir+getfile,"rb")
+            print(getfile)
+        except IOError:
 
-            recvGetLocal = recvData.decode().split("\r\n")
-            pathLocal = recvGetLocal[0].split(" ")
-            getfile = pathLocal[1]
-            htmlRootDir = 'html'
-
-            try:
-                sendfile1 = open(htmlRootDir+getfile,"r")
-                readLines = sendfile1.read()
-                newSocket.send(("HTTP1.1 200 OK \r\n "+ readLines).encode())
-            except:
-
-                newSocket.send(("HTTP1.1 404 NOT FOUND \r\n ").encode())
-
+            responseFirst = "HTTP/1.1 404 Not Found\r\n"
+            responseHeards = "server: myserver \r\n"
+            responseBody = "对访问出错！！"
 
         else:
-            break
-    newSocket.close()
+            filedata = sendfile.read()
+            responseFirst = "HTTP/1.1 200 OK \r\n"
+            responseHeards = "server: myserver \r\n"
+            responseBody = filedata.decode("utf-8")
+
+        response = responseFirst+responseHeards+ "\r\n" +responseBody
+        print(response)
+        newSocket.send(bytes(response,"utf-8"))
+        newSocket.close()
+
+    else:
+        newSocket.close()
+
+
+
+
+
+
 
 
 #开启监听端口，多进程等客户端连接
@@ -36,15 +49,14 @@ if __name__ == '__main__':
 
     serSocket = socket(AF_INET,SOCK_STREAM)
 
-    serSocket.bind(("",8081))
+    serSocket.bind(("",8088))
 
     serSocket.listen(1024)
     while True:
         newSocket,destAddr = serSocket.accept()
-
         p = Process(target=revcfun,args=(newSocket,destAddr))
         p.start()
-    serSocket.close()
+        newSocket.close()
 
 
 
