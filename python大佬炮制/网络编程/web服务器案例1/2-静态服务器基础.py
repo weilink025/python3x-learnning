a8c68e4c2239a20849bdf4f5c8e997a280494966
wade <1272253421@qq.com>
2019-06-29 16:07:38 +0800

    web服务器

---
+++ python大佬炮制/网络编程/web服务器案例1/2-静态服务器基础.py
@@ -0,0 +1,10 @@
+#URL  路径
+#URN  名字（未来）
+#URI  标志符（现在）
+
+#https://cn.bing.com/search?q=cn&pc=MOZI&form=MOZLBR
+
+#?q=cn&pc=MOZI&form=MOZLBR  这个是查询字符段  Query String
+                        #?key1=value1&key2=value2……       （长度是有限制的，不是无限的）
+
+#HTTP是无状态的    如：同一台设备，不同请求服务是当作不同请求来处理的。
