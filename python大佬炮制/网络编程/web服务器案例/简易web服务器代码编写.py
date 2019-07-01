03916e5c359a0415c06da27b94942cce7c771852
zhangwei <1272253421@qq.com>
2019-07-01 16:59:44 +0800

    数据库

--- python大佬炮制/网络编程/web服务器案例/简易web服务器代码编写.py
+++ python大佬炮制/网络编程/web服务器案例/简易web服务器代码编写.py
@@ -5,29 +5,35 @@ from multiprocessing import Process
 #接收数据，并给客户端发送数据
 def revcfun(newSocket,destAddr):
     """处理客户端请求"""
-    while True:
 
+    recvData = newSocket.recv(1024)
+    if recvData:
 
-        recvData = newSocket.recv(1024)
-        if recvData:
+        recvGetLocal = recvData.decode().split("\r\n")
+        pathLocal = recvGetLocal[0].split(" ")
+        getfile = pathLocal[1]
+        htmlRootDir = 'html'
 
-            recvGetLocal = recvData.decode().split("\r\n")
-            pathLocal = recvGetLocal[0].split(" ")
-            getfile = pathLocal[1]
-            htmlRootDir = 'html'
-
-            try:
-                sendfile1 = open(htmlRootDir+getfile,"r")
-                readLines = sendfile1.read()
-                newSocket.send(("HTTP1.1 200 OK \r\n "+ readLines).encode())
-            except:
-
-                newSocket.send(("HTTP1.1 404 NOT FOUND \r\n ").encode())
-
-
+        try:
+            file = open(htmlRootDir + getfile, "rb")
+        except IOError:
+            response_start_line = "HTTP/1.1 404 Not Found\r\n"
+            response_headers = "Server: My server\r\n"
+            response_body = "The file is not found!"
         else:
-            break
-    newSocket.close()
+            file_data = file.read()
+            file.close()
+
+            # 构造响应数据
+            response_start_line = "HTTP/1.1 200 OK\r\n"
+            response_headers = "Server: My server\r\n"
+            response_body = file_data.decode("utf-8")
+
+        response = response_start_line+response_headers+response_body
+        newSocket.send(bytes(response,"utf-8"))
+        newSocket.close()
+
+
 
 
 #开启监听端口，多进程等客户端连接
@@ -35,7 +41,7 @@ if __name__ == '__main__':
 
     serSocket = socket(AF_INET,SOCK_STREAM)
 
-    serSocket.bind(("",8081))
+    serSocket.bind(("",8088))
 
     serSocket.listen(1024)
     while True:
@@ -43,7 +49,7 @@ if __name__ == '__main__':
 
         p = Process(target=revcfun,args=(newSocket,destAddr))
         p.start()
-    serSocket.close()
+        serSocket.close()
 
 
 
