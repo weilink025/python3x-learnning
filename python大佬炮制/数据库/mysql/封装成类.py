fdf4839c2ee096fd58688f749c6cfbfcaeb6c517
wade <1272253421@qq.com>
2019-07-03 16:48:59 +0800

    数据库mysql-python

---
+++ python大佬炮制/数据库/mysql/封装成类.py
@@ -0,0 +1,23 @@
+import MySQLdb
+
+class mysqlHelper():
+    #init方法
+    def __init__(self,host,port,db,user,passwd,charset):
+        self.host = host
+        self.port = port
+        self.db = db
+        self.user = user
+        self.passwd = passwd
+        self.charset = charset
+
+    #连接数据库，创建cusor
+
+    #查一条
+
+    #查多条
+
+    #增
+
+    #删
+
+    #改
