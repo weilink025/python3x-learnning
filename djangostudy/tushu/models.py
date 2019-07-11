49bbbb9e5f830dc9f434e7c5d15907ddb6cbb4d6
wade <1272253421@qq.com>
2019-07-11 16:10:34 +0800

    数据库

--- djangostudy/tushu/models.py
+++ djangostudy/tushu/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+class BookInfo(models.Model):
+    btitle = models.CharField(max_length=20)
+    bpub_data = models.DateField()
+
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=10)
+    hgender = models.BooleanField()
+    hcontent = models.CharField(max_length=1000)
+    hbook = models.ForeignKey(BookInfo,on_delete=models.CASCADE)   #django版本后，外键一定要加on_delete
+
+
 # Create your models here.
+""""
+图书表结构
+表名：BookInfo
+图书名称：btitle
+图书发布时间：bpub_data
+
+英雄表结构设计：
+表名：HeroInfo
+英雄姓名：hname
+英雄性别：hgender
+英雄简介：hcontent
+所属图书：hbook    #外键
+
+"""
