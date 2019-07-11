2906b0e3ff658b12e399513476749a24819ceb77
wade <1272253421qq.com>
2019-07-11 22:36:01 +0800

    django

--- djangostudy/tushu/admin.py
+++ djangostudy/tushu/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class BookInfoAdmin(admin.ModelAdmin):
+    list_display = ['id','btitle','bpub_date']
+
+class HeroInfoAdmin(admin.ModelAdmin):
+    list_display = ['id','hname','hgender','hcontent','hbook']
+
+
+admin.site.register(BookInfo,BookInfoAdmin)
+admin.site.register(HeroInfo,HeroInfoAdmin)
