571ac2f31329af747629edb93db0384d89b4ae89
wade <1272253421qq.com>
2019-07-16 00:04:24 +0800

    工作日志

--- python大佬炮制/django/2  模型进阶/testmodel/testapp/models.py
+++ python大佬炮制/django/2  模型进阶/testmodel/testapp/models.py
@@ -3,7 +3,14 @@ from django.db import models
 # Create your models here.
 class BookInfo(models.Model):
     btitle = models.CharField(max_length=10)
-    bpub_date = models.DateField()
+    bpub_date = models.DateField(db_column=pub_date)
+    bread = models.IntegerField(default=0)
+    bcommet = models.IntegerField(default=0)
+    isDelete = models.BooleanField(default=False)
+    class Meta:
+        db_table='book_wyb'
+        ordering = ['id']  #正序  排序会增加数据库开销
+        ordering = ['-id']  #降序
 
 class HeroInfo(models.Model):
     hname = models.CharField(max_length=10)
